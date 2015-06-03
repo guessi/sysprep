afb52c9268bd41799418d9b034643564c297244d
guessi <guessi@gmail.com>
2015-06-03 09:12:39 +0800

    update git commands

--- post-install-fedora.sh
+++ post-install-fedora.sh
@@ -56,6 +56,13 @@ ${DO_INSTALL} ethtool htop iftop iperf tcpdump
 # git global settings
 git config --global diff.tool colordiff
 git config --global core.editor vim
+git config --global color.ui true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+git config --global alias.lo 'log --oneline --decorate'
 
 # docker
 ${DO_INSTALL} docker
