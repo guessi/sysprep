bff610d09eceef9ca9e9fe74e14dde1b639050db
guessi <guessi@gmail.com>
2016-09-11 00:56:38 +0800

    time to say goodbye, flash

--- post-install-fedora.sh
+++ post-install-fedora.sh
@@ -104,11 +104,6 @@ fi
 # general tools
 ${DO_INSTALL} p7zip p7zip-plugins unzip libcurl
 
-# adobe flash plugin
-${RPM_INSTALL} http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
-${RPM_IMPORT} /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
-${DO_INSTALL} flash-plugin
-
 # multimedia
 ${DO_INSTALL} alsa-plugins-pulseaudio
 
