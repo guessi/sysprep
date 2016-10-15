5080594d76733ecaf3e6d8450c6465a566e14f85
guessi <guessi@gmail.com>
2016-10-16 01:49:40 +0800

    remove annoyed git alias.cp

--- post-install-fedora.sh
+++ post-install-fedora.sh
@@ -77,7 +77,6 @@ git config --global core.editor vim
 git config --global color.ui true
 git config --global alias.ci commit
 git config --global alias.co checkout
-git config --global alias.cp cherry-pick
 git config --global alias.dc 'diff --cached'
 git config --global alias.find '!git ls-files | grep -i'
 git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %C(dim green)%ad%C(reset) %s%C(red)%d%C(reset) [%C(cyan)%an%C(reset)]" --graph --date=short'
