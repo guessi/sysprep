ebf506b3bdf4484a0d8c76a5b880352fb1c7a528
guessi <guessi@gmail.com>
2016-11-20 00:18:05 +0800

    move git preference to separated script

--- bootstrap.fedora.sh
+++ bootstrap.fedora.sh
@@ -81,23 +81,6 @@ ${DO_INSTALL} git gitg tig git-extras
 ${DO_INSTALL} curl colordiff meld vim wget
 ${DO_INSTALL} ethtool htop iftop iperf tcpdump
 
-# git global settings
-git config --global diff.tool colordiff
-git config --global core.editor vim
-git config --global color.ui true
-git config --global color.status true
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.dc 'diff --cached'
-git config --global alias.find '!git ls-files | grep -i'
-git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %C(dim green)%ad%C(reset) %s%C(red)%d%C(reset) [%C(cyan)%an%C(reset)]" --graph --date=short'
-git config --global alias.lo 'log --oneline --decorate'
-git config --global alias.lt '!git describe --tags --abbrev=0 2>/dev/null || echo "not tag defined"'
-git config --global alias.st 'status -s'
-git config --global log.decorate true
-git config --global merge.ff true
-git config --global push.default simple
-
 # create link for git-prompt.sh
 if [ -f /etc/profile.d/git-prompt.sh ] || [ -L /etc/profile.d/git-prompt.sh ]; then
   ${SUDO} rm -vf /etc/profile.d/git-prompt.sh
---
+++ bootstrap.git.preference.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# git global settings
+git config --global diff.tool colordiff
+git config --global core.editor vim
+git config --global color.ui true
+git config --global color.status true
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.dc 'diff --cached'
+git config --global alias.find '!git ls-files | grep -i'
+git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %C(dim green)%ad%C(reset) %s%C(red)%d%C(reset) [%C(cyan)%an%C(reset)]" --graph --date=short'
+git config --global alias.lo 'log --oneline --decorate'
+git config --global alias.lt '!git describe --tags --abbrev=0 2>/dev/null || echo "not tag defined"'
+git config --global alias.st 'status -s'
+git config --global log.decorate true
+git config --global merge.ff true
+git config --global push.default simple
