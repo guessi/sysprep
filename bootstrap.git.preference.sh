fd3194b50b3c90b7354185586b8aa72def4bf536
guessi <guessi@gmail.com>
2020-12-29 15:10:27 +0800

    Misc: regular backup

--- bootstrap.git.preference.sh
+++ bootstrap.git.preference.sh
@@ -4,25 +4,33 @@ USER_NAME="guessi"
 USER_EMAIL="guessi@gmail.com"
 
 # git global settings
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.d 'diff'
-git config --global alias.dc 'diff --cached'
-git config --global alias.find '!git ls-files | grep -i'
-git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %C(dim green)%ad%C(reset) %s%C(red)%d%C(reset) [%C(cyan)%an%C(reset)]" --graph --date=short'
-git config --global alias.lo 'log --oneline --decorate'
-git config --global alias.lt '!git describe --tags --abbrev=0 2>/dev/null || echo "not tag defined"'
-git config --global alias.st 'status -s'
-git config --global core.editor vim
-git config --global core.pager 'less -F -X'
-git config --global diff.tool colordiff
-git config --global log.date rfc-local
-git config --global push.default simple
-git config --global pull.rebase false
 git config --global --bool color.status true
 git config --global --bool color.ui true
 git config --global --bool log.decorate true
 git config --global --bool merge.ff true
+
+git config --global alias.br 'branch'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.d 'diff'
+git config --global alias.dc 'diff --cached'
+git config --global alias.dcs 'diff --cached --stat'
+git config --global alias.ds 'diff --stat'
+git config --global alias.find '!git ls-files | grep -i'
+git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %C(dim green)%ad%C(reset) %s%C(red)%d%C(reset) [%C(cyan)%an%C(reset)]" --graph --date=short'
+git config --global alias.lo 'log --oneline --decorate'
+git config --global alias.lt '!git describe --tags --abbrev=0 2>/dev/null || echo "not tag defined"'
+git config --global alias.s 'show'
+git config --global alias.ss 'show --stat'
+git config --global alias.st 'status -s'
+
+git config --global core.editor vim
+git config --global core.pager 'less -F -X'
+git config --global diff.tool colordiff
+git config --global init.defaultBranch master
+git config --global log.date rfc-local
+git config --global pull.rebase false
+git config --global push.default simple
 
 # github specific configs
 git config --global includeIf."gitdir:~/github/".path ~/.gitconfig-github
